df7bd7df42cd226eb552166d62845e8b7a2c7911
Darian Jason <darianjason@gmail.com>
2021-03-14 23:38:12 +0700

    Add running score display, computer selection display, final result display, play again button

--- rps.js
+++ rps.js
@@ -27,8 +27,8 @@ function getValues(selection) {
 let playerWinCounter = 0;
 let computerWinCounter = 0;
 
-const playerScore=document.querySelector("#player-score");
-const computerScore=document.querySelector("#computer-score");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
 
 function playRound(playerSelection, computerSelection) {
     let playerValue = getValues(playerSelection);
@@ -41,58 +41,87 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (resultValue === 1 || resultValue === -2) {
         playerWinCounter++;
-        playerScore.textContent=playerWinCounter;
+        playerScore.textContent = playerWinCounter;
 
-        return "You win! " + playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
+        return "You win! " + playerSelection + " beats " + computerSelection;
     }
     else if (resultValue === -1 || resultValue === 2) {
         computerWinCounter++;
-        computerScore.textContent=computerWinCounter;
+        computerScore.textContent = computerWinCounter;
 
-        return "You lose! " + computerSelection + " beats " + playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase();
+        return "You lose! " + computerSelection + " beats " + playerSelection;
     }
 }
 
+const page = document.querySelector(".page");
+
+const selectionContainer = document.querySelector(".selection-container");
+const computerSelectionContainer = document.querySelector(".computer-selection-container");
+const computerSelectionText = document.querySelector(".computer-selection")
+
+const finalResultText = document.createElement("div");
+finalResultText.classList.add("final-result-text");
+
+const playAgainButtonContainer = document.createElement("div");
+playAgainButtonContainer.classList.add("play-again-button-container");
+playAgainButtonContainer.classList.add("flex");
+
+const playAgainButton = document.createElement("button");
+playAgainButton.classList.add("play-again-button");
+playAgainButton.textContent = "play again?"
+
 function resetGame() {
     playerWinCounter = 0;
-    playerScore.textContent=0;
+    playerScore.textContent = 0;
 
     computerWinCounter = 0;
-    computerScore.textContent=0;
+    computerScore.textContent = 0;
+
+    // page.removeChild(page.lastChild);
+    // page.removeChild(page.lastChild);
+    // page.removeChild(page.lastChild);
+
+    finalResultText.remove();
+    playAgainButtonContainer.remove();
+
+    computerSelectionText.remove();
+
+    page.appendChild(selectionContainer, page.lastChild);
+    page.appendChild(computerSelectionContainer, page.lastChild);
 }
 
 const playerSelections = Array.from(document.querySelectorAll(".selection"));
 
-const rock=document.querySelector("#rock");
-const paper=document.querySelector("#paper");
-const scissors=document.querySelector("#scissors");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-const selectionName=document.createElement("div");
+const selectionName = document.createElement("div");
 selectionName.classList.toggle("selection-name");
 
 playerSelections.forEach(selection => {
     selection.addEventListener("mouseover", () => {
-        selection.style.transform="scale(1.2, 1.2)";
-        
-        if(selection===rock) {
-            selection.style.fill="lightsalmon";
-            selectionName.textContent="ROCK";
+        selection.style.transform = "scale(1.2, 1.2)";
+
+        if (selection === rock) {
+            selection.style.fill = "lightsalmon";
+            selectionName.textContent = "ROCK";
         }
-        else if(selection===paper) {
-            selection.style.fill="aquamarine";
-            selectionName.textContent="PAPER";
+        else if (selection === paper) {
+            selection.style.fill = "aquamarine";
+            selectionName.textContent = "PAPER";
         }
-        else if(selection===scissors) {
-            selection.style.fill="khaki";
-            selectionName.textContent="SCISSORS";
+        else if (selection === scissors) {
+            selection.style.fill = "khaki";
+            selectionName.textContent = "SCISSORS";
         }
 
         selection.appendChild(selectionName, selection.firstChild);
     });
 
-    selection.addEventListener("mouseout", ()=> {
-        selection.style.transform="scale(1, 1)";
-        selection.style.fill="white";
+    selection.addEventListener("mouseout", () => {
+        selection.style.transform = "scale(1, 1)";
+        selection.style.fill = "white";
         selection.removeChild(selectionName);
     });
 
@@ -102,21 +131,48 @@ playerSelections.forEach(selection => {
 
         let result = playRound(playerSelection, computerSelection);
 
+        computerSelectionText.textContent = computerSelection;
+
+        if (computerSelection === "Rock") {
+            computerSelectionText.style.color = "lightsalmon";
+        }
+        else if (computerSelection === "Paper") {
+            computerSelectionText.style.color = "aquamarine";
+        }
+        else if (computerSelection === "Scissors") {
+            computerSelectionText.style.color = "khaki";
+        }
+
+        computerSelectionText.remove();
+        computerSelectionContainer.appendChild(computerSelectionText, computerSelectionContainer.firstChild);
+
         console.log("player selection: " + playerSelection + "\ncomputer selection: " + computerSelection);
         console.log(result);
         console.log("player wins: " + playerWinCounter + "\ncomputer wins: " + computerWinCounter);
 
-        if(playerWinCounter===5 || computerWinCounter===5) {
+        if (playerWinCounter === 5 || computerWinCounter === 5) {
+            page.removeChild(selectionContainer);
+
             if (playerWinCounter === 5) {
+                finalResultText.textContent = "YOU WON!";
+
                 console.log("YOU WON");
             }
             else if (computerWinCounter === 5) {
+                finalResultText.textContent = "YOU LOST..."
+
                 console.log("YOU LOST");
             }
 
-            // CSS display: hide selection div's (foreach) to show result
+            page.insertBefore(finalResultText, computerSelectionContainer);
 
-            resetGame();
+            page.removeChild(computerSelectionContainer);
+            page.appendChild(playAgainButtonContainer);
+            playAgainButtonContainer.appendChild(playAgainButton);
+
+            playAgainButton.addEventListener("click", () => {
+                resetGame();
+            });
         }
     });
 });
